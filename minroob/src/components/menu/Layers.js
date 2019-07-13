a36170f4d5c5062307aa58e07c3eba3caecabf21
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-07-13 10:19:23 +0430

    Solve miniroob

--- minroob/src/components/menu/Layers.js
+++ minroob/src/components/menu/Layers.js
@@ -4,22 +4,33 @@
  */
 import React from 'react';
 import './Layers.css';
-import {Board} from "../board/Board";
-import {CELL_MARGIN, CELL_SIZE} from "../../constants";
+import { Board } from '../board/Board';
+import { CELL_MARGIN, CELL_SIZE } from '../../constants';
 
 export class Layers extends React.PureComponent {
-    render() {
-        const {n, board} = this.props;
-        return <div className="layers" style={{paddingTop: CELL_MARGIN/2}}>
-            {board.map((board2d, index) => <div key={index} style={{
-                width: CELL_SIZE,
-                height: CELL_SIZE ,
-                overflow: 'hidden'
+  render() {
+    const { n, board, currentLayer } = this.props;
+    return (
+      <div className="layers" style={{ paddingTop: CELL_MARGIN / 2 }}>
+        {board.map((board2d, index) => (
+          <div
+            onClick={() => this.props.setCurrentLayer(index)}
+            key={index}
+            style={{
+              width: CELL_SIZE,
+              height: CELL_SIZE,
+              overflow: 'hidden'
             }}>
-                <Board board={board2d} n={n} layer={index} type={'mini'}/>
-            </div>)}
-        </div>;
-
-
-    }
+            <Board
+              className={currentLayer === index ? 'current' : ''}
+              board={board2d}
+              n={n}
+              layer={index}
+              type={'mini'}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
